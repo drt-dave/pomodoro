1aa680d55971b4c8d5fb992e0e46786f3cc15dee
root <root@localhost.localdomain>
2025-10-18 05:16:12 +0000

    first commit

---
+++ src/types/pomodoro.types.ts
@@ -0,0 +1,18 @@
+// Basic types
+export type PomodoroMode = 'work' | 'break';
+
+export interface PomodoroState {
+  tag: string;
+  mode: PomodoroMode;
+  timeLeft: number;
+  isRunning: boolean;
+  defaultWorkTime: number;
+  defaultBreakTime: number;
+}
+
+export interface PomodoroSession {
+  tag: string;
+  duration: number;
+  timestamp: number;
+  completed: boolean;
+}
